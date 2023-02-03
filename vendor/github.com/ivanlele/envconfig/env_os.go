78d922eeeedf677500f4dff48447ab74ea158e5d
ivanlele <ivanlele402@gmail.com>
2023-02-03 09:46:42 +0000

    move the config to env

---
+++ vendor/github.com/ivanlele/envconfig/env_os.go
@@ -0,0 +1,15 @@
+//go:build appengine || go1.5
+// +build appengine go1.5
+
+package envconfig
+
+import "os"
+
+func lookupEnv(key string) (string, bool) {
+	value, found := os.LookupEnv(key)
+	if found {
+		os.Unsetenv(key)
+	}
+
+	return value, found
+}
